ff0c757f911d93c500461c9370ff29bc3846ee3e
Ron <thechosenweak@gmail.com>
2022-09-17 22:31:28 +0800

    update

--- mobile/src/navigations/RootStackNavigation.js
+++ mobile/src/navigations/RootStackNavigation.js
@@ -3,26 +3,29 @@ import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Initializer from "./Initializer";
 import LoginScreen from './../screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 export default function RootStackNavigation(){
 
-    return(
-        <SafeAreaView style={{flex: 1}}>
-            <NavigationContainer>
-                <Stack.Navigator 
-                    screenOptions={{
-                        headerShown: false
-                    }}
-                >
-                    <Stack.Screen
-                        name="LoginScreen"
-                        component={LoginScreen}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaView>
+    return (
+        <Initializer>
+            <SafeAreaView style={{flex: 1}}>
+                <NavigationContainer>
+                    <Stack.Navigator 
+                        screenOptions={{
+                            headerShown: false
+                        }}
+                    >
+                        <Stack.Screen
+                            name="LoginScreen"
+                            component={LoginScreen}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+        </Initializer>
     )
 }
