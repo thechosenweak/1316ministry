ec4a1e5a2bccc227aece1fbe04033fb957f540b2
Ron <thechosenweak@gmail.com>
2023-01-22 21:36:43 +0800

    update

--- mobile/src/components/AppTabBar/index.js
+++ mobile/src/components/AppTabBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
 import Ripple from 'react-native-material-ripple';
 
 const AppTabBar = ({ 
